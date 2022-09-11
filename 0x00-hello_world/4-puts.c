12c175b2428491d8ec8db48ba24dfb47cf3d2a10
Kingsley James <drkings.kj@gmail.com>
2022-09-11 01:29:52 -0700

    Task 0x04. C - Hello, World Edit 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdio.h>
-#include <string.h>
 
 /**
  * main - Entry point
@@ -10,9 +7,7 @@
  */
 int main(void)
 {
-
         /* initializing strings */
         char str1[] = ("\"Programming is like building a multilingual puzzle");
-        puts(str1);
         return 0;
 }
