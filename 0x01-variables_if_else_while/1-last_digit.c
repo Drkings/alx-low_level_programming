86766897f8a266e9a6118a07c37dca35e911936d
Kingsley James <drkings.kj@gmail.com>
2022-09-11 13:57:25 -0700

    Alx Task 1 0x01. C - Variables, if, else, while 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 /**
- * *main - Prints the last digit of random number
- * *
- * *Return: Return 0
- * */
+ * main - Prints the last digit of random number
+ * 
+ * Return: Return 0
+ */
 int main(void)
 {
 	/* where l = last digit of the random numbers */
