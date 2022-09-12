ef291e42917cec02c48d1a34631c7d7d57e28e21
Kingsley James <drkings.kj@gmail.com>
2022-09-11 21:21:57 -0700

    Alx Task 5 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - all single numbers of base ten starting from zero
+ *
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	int num;
+
+	for (num == '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
