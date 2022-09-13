034a97567bc24d6cc6d8dc54829c6f413c35b6ed
Kingsley James <drkings.kj@gmail.com>
2022-09-12 17:45:00 -0700

    Alx Task 9 0x01-variables_if_else_while 3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-/** main - prints all possible combinations of single-digit numbers
+/**
+ * main - prints all possible combinations of single-digit numbers
  *
  * Return: Always 0.
  */
@@ -19,5 +20,5 @@ int main(void)
 	}
 	putchar('\n');
 
-	return (o)
+	return (0);
 }
