a538d7ba874c432241e354595779752ec64d0909
Kingsley James <drkings.kj@gmail.com>
2022-09-14 19:03:33 -0700

    Alx Task 1 0x02-Functions, Nested Loops Edit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
  * print_alphabet - program that prints alphabet
  *
  * Return: void
  */
-
 void print_alphabet(void)
 {
-	int i;
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+char i;
+for (i = 'a'; i <= 'z'; i++)
+
+_putchar(i);
+_putchar('\n');
 }
