d0b789ca4935261c0327c07c809ba3fafd03901b
Kingsley James <drkings.kj@gmail.com>
2022-09-15 02:43:43 -0700

    Alx Task 11 0x02. C - Functions, nested loops update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - function that prints numbers from n to 98
@@ -8,7 +9,8 @@
  * Return: ALways 0
  */
 
-void print_to_98(int n);
+void print_to_98(int n)
+
 {
 if (n >= 98)
 {
