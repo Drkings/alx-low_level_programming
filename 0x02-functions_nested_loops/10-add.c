8cedbf0e3a6b3bdb24b9fdb75e9742878a3c8ee5
Kingsley James <drkings.kj@gmail.com>
2022-09-14 23:25:12 -0700

    Alx Task 10 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints adds two integers
+ *@i: print int i
+ *@j: print int j
+ * Return: Always 0
+ */
+
+int add(int i, int j)
+{
+return (i + j);
+}
+
