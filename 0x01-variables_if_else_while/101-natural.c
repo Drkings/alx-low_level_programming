ffab713c7172daa8fb9de3f46316ef6a721cb529
Kingsley James <drkings.kj@gmail.com>
2022-09-15 20:51:02 -0700

    Alx Task 13 0x02. C - Functions, nested loop

---
+++ 0x01-variables_if_else_while/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - lists all natuarl numbers bele 1024(excluded, multiples of 3 and 5
+ *
+ * Return: Always : 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((1 % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
