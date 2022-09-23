9c19519fd7981af7542ae66d2f79dde97fdc23c9
Kingsley James <drkings.kj@gmail.com>
2022-09-23 04:32:56 +0000

    Alx Task 0x06. C - More pointers, arrays and strings update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,5 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
-
-int _putchar(char c);
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 char *_strcat(char *dest, char *src);
 
@@ -26,6 +21,10 @@ char *rot13(char *);
 
 void print_number(int n);
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int _putchar(char c);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
 
 #endif
