7c2e67df5d3efb230b20f192e2785a2763392ce1
Kingsley James <drkings.kj@gmail.com>
2022-10-22 03:14:17 +0000

    Alx Task: 0x0F. C - Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,19 @@
-
-
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter
- * on each element of an array
- * @array: array to iterate over
- * @size: size of the array
- * @action: pointer to function used
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array.
+ * @array: input integer array.
+ * @size: size of the array.
+ * @action: pointer to the function.
+ *
+ * Return: no return.
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || action == NULL)
-		return;
+	unsigned int i;
 
-	while (size-- > 0)
-	{
-		action(*array);
-		array++;
-	}
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
